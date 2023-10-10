d849966f0e84fd20119f5f27eb554a1a1b398b6f
Abhishek Anand <anandabhishek3607@gmail.com>
2023-10-10 15:41:05 +0530

    working with non string props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 // import Chicken from "./Chicken";
 import Greeter from "./Greeter";
+import Die from "./Die";
 
 function App() {
   return (
     <div>
-      <Greeter person="Bill" />
-      <Greeter person="Batman" />
-      <Greeter person="Kalia" />
+      <Greeter person="Bill" from="Colt!" />
+      <Greeter person="Batman" from="Doga!" />
+      <Greeter person="Kalia" from="Dhruv!" />
+      <Die numSides={20} />
+      <Die numSides={11} />
+      <Die numSides={26} />
     </div>
   );
 }
---
+++ src/Die.jsx
@@ -0,0 +1,10 @@
+export default function Die({ numSides }) {
+  const roll = Math.floor(Math.random() * numSides) + 1;
+  return (
+    <>
+      <p>
+        {numSides}-sided Die Roll {roll}
+      </p>
+    </>
+  );
+}
